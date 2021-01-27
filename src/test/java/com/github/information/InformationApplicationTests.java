e27e114f44af953b7c0c1b684784245c6d7992fa
mxie <836728902@qq.com>
2021-01-27 22:55:50 +0200

    Basic set up

---
+++ src/test/java/com/github/information/InformationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.information;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InformationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
