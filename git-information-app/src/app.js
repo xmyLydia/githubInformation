8ae75f2ee0c7bc5bd8f73817969adfd089fa7508
mxie <836728902@qq.com>
2021-02-01 02:51:15 +0200

    Build and cleaning.

--- git-information-app/src/app.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import IndecisionApp from './components/IndecisionApp';
-import './style/styles.scss';
-import 'normalize.css/normalize.css';
-
-ReactDOM.render(<IndecisionApp/>, document.getElementById('app'));
